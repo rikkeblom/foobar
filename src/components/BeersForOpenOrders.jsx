1a940ef0a66a4d062679428736933050f2416085
emily <emil7809@gmail.com>
2021-12-04 16:37:52 +0100

    images not working, but somthing is happening

--- src/components/BeersForOpenOrders.jsx
+++ src/components/BeersForOpenOrders.jsx
@@ -9,18 +9,31 @@ export default function BeersForOpenOrders(props) {
         return beer.beer === name;
       }
     });
+    
   }
   let tapNumber = tap.id + 1;
+  
+  getImagesForBeer();
+  function getImagesForBeer() {
+    console.log("Get images for beer");
+
+    if (props.beer.beer === "Fairy Tale Ale") {
+       document.querySelector(".beer_lable").src="src/img/beer/fairytaleale.png";
+     } else if (props.beer.beer === "GitHop") {
+       document.querySelector(".beer_lable").src="src/img/beer/githop.png";
+     }
+
+  }
 
   return (
     <article className="lable_article">
-      <img src="#" alt="BeerLabel" />
+       <img className="beer_lable" src="" alt="beer_lable" />
       <div>
         <h4>{props.beer.beer}</h4>
         <div className="line_it_up">
           <p>Amount</p>
           <div className="line1"></div>
-          <p>{props.beer.amount}</p>
+          <p>x{props.beer.amount}</p>
         </div>
         <div className="line_it_up">
           <p>Tap</p>
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,7 @@
 export default function Notification() {
   return (
     <section className="alarm">
-      <img src="src/img/icons/bell_icon.svg" alt="" />
+      <img src="src/img/icons/bell_icon.svg" alt="bell_icon" />
     </section>
   );
 }
