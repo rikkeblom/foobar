20218a5725639c71f5f459d9e764fc9b4fe657ac
emily <emil7809@gmail.com>
2021-12-07 11:05:54 +0100

    main conteckt added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,10 @@ import ColleaguesSection from "./ColleagueSection";
 export default function Main(props) {
   return (
     <main>
+      <div className="served_queue_container">
       <BeingServed beers={props.beers} bartenders={props.data.bartenders} serving={props.data.serving} taps={props.data.taps}></BeingServed>
       <InQueue beers={props.beers} queue={props.data.queue} taps={props.data.taps}></InQueue>
+      </div>
       <TapStatus beers={props.beers} taps={props.data.taps}></TapStatus>
       <ColleaguesSection bartenders={props.data.bartenders}></ColleaguesSection>
     </main>
