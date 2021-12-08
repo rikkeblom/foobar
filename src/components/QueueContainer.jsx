2f407ac214ee2c08b5919a599aafd13523e6903a
emily <emil7809@gmail.com>
2021-12-08 17:45:36 +0100

    tweeks made

--- src/components/QueueContainer.jsx
+++ src/components/QueueContainer.jsx
@@ -5,5 +5,5 @@ export default function QueueContainer(props) {
   //same system as for the being served section we need to map through the array
   const mapped = props.queue.map((order) => <QueueOrder beers={props.beers} taps={props.taps} {...order} key={order.id} />);
 
-  return <section>{mapped}</section>;
+  return <section className="queue_container">{mapped}</section>;
 }
