4871fc17988d936281a9996751de690ecc7baca7
Rikke Blom <rikk957v@stud.kea.dk>
2021-12-11 22:23:23 +0100

    flex-wrap on tap instead for centered on large screens

--- src/components/QueueContainer.jsx
+++ src/components/QueueContainer.jsx
@@ -5,8 +5,9 @@ export default function QueueContainer(props) {
   //same system as for the being served section we need to map through the array
 
   //adding a static queued order for styling purposes
-  const dataPlusFakeOrder = props.queue.concat({ id: 1000000, order: ["Ruined Childhood", "Steampunk", "Steampunk"] });
+  // const dataPlusFakeOrder = props.queue.concat({ id: 1000000, order: ["Ruined Childhood", "Steampunk", "Steampunk"] });
+  // const mapped = dataPlusFakeOrder.map((order) => <QueueOrder beers={props.beers} taps={props.taps} {...order} key={order.id} />);
 
-  const mapped = dataPlusFakeOrder.map((order) => <QueueOrder beers={props.beers} taps={props.taps} {...order} key={order.id} />);
+  const mapped = props.queue.map((order) => <QueueOrder beers={props.beers} taps={props.taps} {...order} key={order.id} />);
   return <section className="queue_container">{mapped}</section>;
 }
