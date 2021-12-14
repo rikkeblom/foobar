eee3eec6bd4a228ed3399de97bd8b5aa75ae8424
Rikke Blom <rikk957v@stud.kea.dk>
2021-12-14 09:40:32 +0100

    2.1: static asset handling on open queue

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,9 @@
+import bellIconURL from "/img/icons/bell_icon.svg";
+
 export default function Notification() {
   return (
     <section className="alarm">
-      <img src="/img/icons/bell_icon.svg" alt="" />
+      <img src={bellIconURL} alt="" />
     </section>
   );
 }
