26b498d8209bc7fd0ce46dd372a17181fd5f018d
Rikke Blom <rikk957v@stud.kea.dk>
2021-12-14 18:51:48 +0100

    switched to webp format - saved appr 90%

--- src/components/BeersForQueuedOrders.jsx
+++ src/components/BeersForQueuedOrders.jsx
@@ -1,13 +1,13 @@
-import elhefeLabelURL from "/img/labels/elhefe.png";
-import fairytalealeLabelURL from "/img/labels/fairytaleale.png";
-import githobLabelURL from "/img/labels/githop.png";
-import hollabacklagerLabelURL from "/img/labels/hollabacklager.png";
-import hoppilyeverafterLabelURL from "/img/labels/hoppilyeverafter.png";
-import mowintimeLabelURL from "/img/labels/mowintime.png";
-import row26LabelURL from "/img/labels/row26.png";
-import ruinedchildhoodLabelURL from "/img/labels/ruinedchildhood.png";
-import sleighrideLabelURL from "/img/labels/sleighride.png";
-import steampunkLabelURL from "/img/labels/steampunk.png";
+import elhefeLabelURL from "/img/labels/elhefe.webp";
+import fairytalealeLabelURL from "/img/labels/fairytaleale.webp";
+import githobLabelURL from "/img/labels/githop.webp";
+import hollabacklagerLabelURL from "/img/labels/hollabacklager.webp";
+import hoppilyeverafterLabelURL from "/img/labels/hoppilyeverafter.webp";
+import mowintimeLabelURL from "/img/labels/mowintime.webp";
+import row26LabelURL from "/img/labels/row26.webp";
+import ruinedchildhoodLabelURL from "/img/labels/ruinedchildhood.webp";
+import sleighrideLabelURL from "/img/labels/sleighride.webp";
+import steampunkLabelURL from "/img/labels/steampunk.webp";
 import redTapIconURL from "/img/icons/red_tap.png";
 
 //props receives beer(.amount & .beer) og taps
